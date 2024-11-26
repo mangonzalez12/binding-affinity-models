d2846941cadea5a08d827bbfbfb2e41f9d997ae6
mangonzalez12 <mangonzalez12@gmail.com>
2024-11-26 14:00:41 -0600

    Add function to read files

---
+++ scripts/utils.py
@@ -0,0 +1,10 @@
+import pandas as pd
+def load_data(filepath):
+    """ Load datasets from parquet files using the pd_parquet function.
+    Args: 
+        filepath(str): Path to the parquet file.
+    Returns: 
+        pandas dataframe: loaded dataframe
+    """
+
+    return pd.read_parquet(filepath)
